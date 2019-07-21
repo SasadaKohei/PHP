ea317e063a440cfc254b30c18528d3c8869118db
sasadakouhei <c501937014@tokushima-u.ac.jp>
2019-07-21 17:33:32 +0900

    (2/9)

---
+++ samplecake/src/Controller/PersonsController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Controller\AppController;
+
+class PersonsController extends AppController
+{
+    public function add()
+    {
+        if ($this->request->is('post')) {
+            $person = $this->Persons->newEntity();
+            $person = $this->Persons->patchEntity($person, $this->request->getData());
+            if ($this->Persons->save($person)) {
+                return $this->redirect(['action' => 'index']);
+            }
+        }
+    }
+}
