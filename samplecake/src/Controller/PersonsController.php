18e70d308e7fee2190578165833b8cba8b398e73
sasadakouhei <c501937014@tokushima-u.ac.jp>
2019-07-30 01:52:48 +0900

    (6/6)

--- samplecake/src/Controller/PersonsController.php
+++ samplecake/src/Controller/PersonsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager; // このuseを追加
 
 class PersonsController extends AppController
 {
@@ -54,22 +55,35 @@ class PersonsController extends AppController
     }
 
 //    レコードの検索
+//    public function find() {
+//        $persons = [];
+//        if ($this->request->is('post')) {
+//            $find = $this->request->data['find'];
+//            $query = $this->Persons->find();
+//            $exp = $query->newExpr();
+//            $fnc = function($exp, $f) {
+//                return $exp
+//                    ->isNotNull('name')
+//                    ->isNotNull('mail')
+//                    ->gt('age',0)
+//                    ->in('name', explode(',',$f));
+//            };
+//            $persons = $query->where($fnc($exp,$find));
+//        }
+//        $this->set('persons', $persons);
+//        $this->set('msg', null);
+//    }
+
+
     public function find() {
+        $this->set('msg', null);
         $persons = [];
         if ($this->request->is('post')) {
             $find = $this->request->data['find'];
-            $query = $this->Persons->find();
-            $exp = $query->newExpr();
-            $fnc = function($exp, $f) {
-                return $exp
-                    ->isNotNull('name')
-                    ->isNotNull('mail')
-                    ->gt('age',0)
-                    ->in('name', explode(',',$f));
-            };
-            $persons = $query->where($fnc($exp,$find));
+            $connection = ConnectionManager::get('default');
+            $query = 'select * from persons where ' . $find;
+            $persons = $connection->query($query)->fetchAll();
         }
         $this->set('persons', $persons);
-        $this->set('msg', null);
     }
 }
